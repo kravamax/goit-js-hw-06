84e5ed8d386d7f2d6e14505f4739da9143e59325
Maksym Kravchenko <maks.cross@gmail.com>
2022-02-01 12:05:48 +0200

    Решение 4 задания

---
+++ js/task-04.js
@@ -0,0 +1,21 @@
+const refs = {
+  decrement: document.querySelector('[data-action="decrement"]'),
+  increment: document.querySelector('[data-action="increment"]'),
+  value: document.querySelector("#value"),
+};
+
+let counterValue = 0;
+
+increment();
+decrement();
+
+refs.decrement.addEventListener("click", decrement);
+refs.increment.addEventListener("click", increment);
+
+function decrement() {
+  refs.value.textContent = counterValue -= 1;
+}
+
+function increment() {
+  refs.value.textContent = counterValue += 1;
+}
