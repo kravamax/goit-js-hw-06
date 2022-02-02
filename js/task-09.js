2ef52e9167b224c919348b8ce5bf9076625c2879
Maksym Kravchenko <maks.cross@gmail.com>
2022-02-02 16:06:22 +0200

    Решение 9 задания

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,22 @@
+const refs = {
+  changeColorBtn: document.querySelector(".change-color"),
+  body: document.querySelector("body"),
+  textColor: document.querySelector(".color"),
+};
+
+getRandomHexColor();
+
+refs.changeColorBtn.addEventListener("click", changeBackgroundColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeBackgroundColor() {
+  const color = getRandomHexColor();
+
+  refs.body.style.backgroundColor = color;
+  refs.textColor.innerText = color;
 }
