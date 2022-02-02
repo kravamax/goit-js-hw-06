b16c7640663fe94be889b6a49e4a6406bb6090e2
Maksym Kravchenko <maks.cross@gmail.com>
2022-02-02 14:40:35 +0200

    Решение 8 задания

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const refs = {
+  form: document.querySelector(".login-form"),
+};
 
+refs.form.addEventListener("submit", formSubmit);
+
+function formSubmit(e) {
+  e.preventDefault();
+
+  const formData = e.currentTarget.elements;
+  const {
+    elements: { email, password },
+  } = e.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("All fields must be filled");
+  }
+
+  console.log(formData);
+
+  e.currentTarget.reset();
+}
