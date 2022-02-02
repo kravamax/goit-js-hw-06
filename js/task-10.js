ffdf3e2fae206dc2c0fae1a26a2fe4a03b50fc1c
Maksym Kravchenko <maks.cross@gmail.com>
2022-02-02 21:37:58 +0200

    Решение 10 задания

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,37 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  input: document.querySelector("[type=number]"),
+  createButton: document.querySelector("[data-create]"),
+  destroyButton: document.querySelector("[data-destroy]"),
+  boxesContainer: document.querySelector("#boxes"),
+};
+
+let sideLength = 30;
+const valueAddLength = 10;
+
+refs.createButton.addEventListener("click", createBoxes);
+refs.destroyButton.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  amount = refs.input.value;
+
+  let blockMarkup = "";
+
+  for (let i = 0; i < amount; i += 1) {
+    const randomColor = getRandomHexColor();
+
+    blockMarkup += `<div style ="width:${sideLength}px; height:${sideLength}px; background-color:${randomColor}; "></div>`;
+
+    sideLength += valueAddLength;
+  }
+
+  return refs.boxesContainer.insertAdjacentHTML("afterbegin", blockMarkup);
+}
+
+function destroyBoxes() {
+  refs.boxesContainer.innerHTML = "";
+  sideLength = 30;
+}
